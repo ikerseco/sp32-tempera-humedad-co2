1cb04129d757f0b2301c370a5681d7f45049ac8a
ikerseco <garrantsitsua@gmail.com>
2020-11-09 22:20:39 +0100

    conponiendo MQTT

--- proyectTHCho2.ino
+++ proyectTHCho2.ino
@@ -1,31 +1,40 @@
-
-
-
-
 /*
  *  This sketch demonstrates how to scan WiFi networks.
  *  The API is almost the same as with the WiFi Shield library,
  *  the most obvious difference being the different file you need to include:
  */
-#include "WiFi.h"
+#include  <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-#include <ArduinoJson.h>
-
-int LED17 = 17;
+#include <EspMQTTClient.h>
 
 
-const char* ssid = "EUSKALTEL_D0011768";
-const char* password = "HGWMUUWJ";
 
-//json
-char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+//pin LED digital pin 0
+int PIN0 = 0;
+//pin DTH11 digital pin 17,
+int PIN17 = 17;
+//pin buzzer digital pin 16
+int PIN16 = 16;
+//pin MQ-2 analog pin 36
+int PIN36 = 36;
 
-//pins func 17,
+
+//mqtt
+EspMQTTClient client(
+  "EUSKALTEL_D0011768",
+  "HGWMUUWJ",
+  "192.168.0.14",  // MQTT Broker server ip
+  "TestClient",// Client name that uniquely identify your device
+  1883
+  );
 
 
-//sensor DHT1 gpio34
-DHT dht1(17,DHT11); 
+
+
+
+//sensor DHT1 gpio17
+DHT dht1(PIN17,DHT11); 
 float ten1 = 00.0;
 float humi1 = 00.0;
 
@@ -51,11 +60,7 @@ String scanHumidity1(){
      return String("none");
   }
 }
-
-
-
-
-
+/*
 //wifiConectfucn
 void wifiConect(){
     Serial.println();
@@ -73,46 +78,103 @@ void wifiConect(){
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
     if(WiFi.status() == WL_CONNECTED){
-      digitalWrite(LED17, HIGH);
+      digitalWrite(PIN0, HIGH);
     }
 }
-//gpio egiaztatzeko funtzioa
-void led(String led,boolean metode){
+
+
+*/
+//gpio egiaztatzeko led funtzioa
+void led(int led,boolean metode){
   if(metode == true){
-      digitalWrite(17, HIGH);
+      digitalWrite(led, HIGH);
   }else{
-      digitalWrite(17, LOW);
+      digitalWrite(led, LOW);
    }
 }
+
+//buzzer korreonte actiboaren alarma funtzioa, pin analog 1
+void buzzerAc(int pin,boolean metodo){
+    if(metodo == true){
+      digitalWrite(pin, HIGH);
+  }else{
+      digitalWrite(pin, LOW);
+   }
+} 
+
+
+//buzzer korreonte alternoaren alarma funtzioa, pin analog 16
+void buzzerAL(int pin,boolean metodo,int Max,int Min){
+  if(metodo == true){
+    ledcSetup(0, Max, Min); // setup beeper
+    ledcAttachPin(pin, 0); // attach beeper
+    ledcWriteTone(0, 200); // play tone 
+  }else{
+    ledcAttachPin(pin, 1);
+  }
+} 
+
+
+//MQ-2 funtzioa
+boolean MQ2(int Media,int buzzer,int analog){
+   int analogSensor = analogRead(analog);
+   delay(500);
+   if (analogSensor > Media){
+      Serial.println("GAS:"+ String(analogSensor));
+      buzzerAL(buzzer,true,200,100);
+      return boolean(true);
+    }
+    else{
+      Serial.println("GAS:"+ String(analogSensor));
+      buzzerAL(buzzer,false,200,100); 
+      return boolean(false);
+    }
+}
+
+
+
 void setup()
 {
     Serial.begin(115200);
-    //pinMode(17, OUTPUT); 
-    DynamicJsonDocument doc(1024);
-    
-    doc["sensor"] = "gps";
-    doc["time"]   = 1351824120;
-    doc["data"][0] = 48.756080;
-    doc["data"][1] = 2.302038;
-    
-    serializeJson(doc, Serial);;
+     client.enableDebuggingMessages();//
+    //led digital
+    pinMode(PIN0,OUTPUT);
+    //buzzer corriente activa Digital OUTPUT
+    pinMode(PIN16,OUTPUT);
+    //MQ-2 sensor Analogic INPUT
+    pinMode(36,INPUT);
+    //pinMode(PIN4,OUTPUT);
     //conesionWifi
-   // wifiConect();
+    delay(5000);
+    client.loop();
 }
+
+
+void onConnectionEstablished() {
+  client.subscribe("test", [] (const String &payload)  {
+    Serial.println(payload);
+    delay(2000);
+    String tempe1 = scanTenperature1();
+    delay(2000);
+    String humi1 = scanHumidity1();
+    delay(2000);
+    boolean datMq1 = MQ2(1100,PIN16,PIN36);
+    delay(500);
+    Serial.println("DHT1 tenperature: "+tempe1);
+    Serial.println("DHT1 humidity: "+humi1);
+    String DatTotal = tempe1+"/"+humi1+"/"+datMq1;
+    Serial.println(DatTotal);
+    //client.publish("test",DatTotal);
+  });
+}
+
 
 void loop()
 {
-    delay(5000);
-    String tempe1 = scanTenperature1();
-    delay(5000);
-    String humi1 = scanHumidity1();
-   
-    //Serial.println("bai");
-    Serial.println("DHT1 tenperature: "+tempe1);
-    Serial.println("DHT1 humidity: "+humi1);
-  //digitalWrite(17, HIGH); 
-    delay(5000);
-  //Serial.println("bai");                          
-  //digitalWrite(17, LOW);  
-  //delay(500);
+    client.loop();
+   // delay(5000);
+    //boolean datMq1 = MQ2(1100,PIN16,PIN36);
+    //int analogSensor = analogRead(36);
+    //Serial.println(analogSensor);
+   // delay(5000);
 }
